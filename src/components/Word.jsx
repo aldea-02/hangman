b5538acad57ecf68e7e984e35e9c6aaa8e2414f0
aldea02 <aldea02@icloud.com>
2022-11-05 14:28:36 +0200

    added body parts and word component

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,4 +1,13 @@
 function Word() {
-	return <div>Word</div>
+	const word = 'word'
+	return (
+		<div className='flex gap-1 text-8xl font-bold uppercase tracking-wider'>
+			{word.split('').map((letter, index) => (
+				<span className='border-b-2 border-black' key={index}>
+					<span>{letter}</span>
+				</span>
+			))}
+		</div>
+	)
 }
 export default Word
